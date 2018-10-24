04c925bf2cd043914dc7a5952f086e35cb25eac0
Mridula Shanker <Mridula Shanker@LAPTOP-T2PHJ0FM>
2018-10-23 20:25:02 -0400

    Sept 14 2018

---
+++ Variable/src/Variable.java
@@ -0,0 +1,29 @@
+
+public class Variable 
+{
+
+	public static void main(String[] args) 
+	{
+		String name = "Mridula";
+		System.out.println("My name is "+ name);
+		
+		int width = 2;
+		int length = 4;
+		int rectArea = width * length;
+		System.out.println("The area of the rectangle is " + rectArea);
+		
+		int radius = 4;
+		double pi = 3.14;
+		double circum = 2* pi * radius;
+		System.out.println("The circumfrence of the circle is " + circum);
+		
+		double circleArea = pi * (radius*radius);
+		System.out.println("The area of the circle is " + circleArea);
+		
+		
+		
+		
+
+	}
+
+}
